e4d0bebf1633b54bc795d4205ae2c32877bb67a0
cemkoc <cemkoc95@gmail.com>
2014-02-07 21:44:36 -0800

    web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
+
 var app = express();
-app.use(express.logger());
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+ var html = fs.readFileSync(htmlfile).toString();
+ response.send(html);
 });
 
 var port = process.env.PORT || 5000;
